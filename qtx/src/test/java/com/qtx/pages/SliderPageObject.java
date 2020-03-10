fef580ed633c35b59bfb3bf6de71fded5c7ac05d
RajKapadia <rajkapadia1997@gmail.com>
2020-03-09 23:18:04 -0700

    changed the slider logic

--- qtx/src/test/java/com/qtx/pages/SliderPageObject.java
+++ qtx/src/test/java/com/qtx/pages/SliderPageObject.java
@@ -5,13 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class SliderPageObject extends SuperPageObject{
-	
-	
+public class SliderPageObject extends SuperPageObject {
+
 	public SliderPageObject(WebDriver driverInstance, String baseUrl) {
 		super(driverInstance, baseUrl);
 	}
-	
+
 	public SliderPageObject Navigate() {
 		String url = "/horizontal_slider";
 
@@ -20,17 +19,16 @@ public class SliderPageObject extends SuperPageObject{
 	}
 
 	public String SetSliderValue() {
+		//grabbing the slider element
 		WebElement slider = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/input"));
-		Actions action = new Actions(driver);
-		action.dragAndDropBy(slider, 100, 0).perform();
-		WebElement rangeValue = driver.findElement(By.id("range"));
-		String value = rangeValue.getText();
-		
+        
+		int width = slider.getSize().getWidth();  //getting the width of the slider
+		Actions action = new Actions(driver);  
+		action.dragAndDropBy(slider, (width * 100)/100, 0).perform();  //perform drag and drop 
+		WebElement rangeValue = driver.findElement(By.id("range"));   
+		String value = rangeValue.getText();  //getting the range value
+
 		return value;
 	}
-
-	
-	
-	
 
 }
